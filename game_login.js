d4a1fdae801290cd6512246d7273010c8287085a
BiazinhaCat <104387462+BiazinhaCat@users.noreply.github.com>
2022-09-20 19:53:44 +0100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function addUser() {
+    
+    player1_Name = document.getElementById("player1NameInput").value;
+    player2_Name = document.getElementById("player2NameInput").value;
+
+    localStorage.setItem("player1_Name", player1_Name);
+    localStorage.setItem("player2_Name", player2_Name);
+
+    window.location = "game_page.html"
+
+}
